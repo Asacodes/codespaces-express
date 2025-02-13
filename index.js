8b45203d523a7307f5fa65b75bbdaac5a1c22c2d
Asacodes <aaronsyasil@gmail.com>
2025-02-13 14:24:30 +0000

    a

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 4444
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+app.use(cors())
+
+let corsOptions = {
+  origin: ['https://localhost:3000']
+}
+
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/submit', cors(corsOptions), (req, res) => {
+    console.log(req.body)
+    res.json({
+      code: 'buang'
+    })
 })
 
 app.listen(port, () => {
